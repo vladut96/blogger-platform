5afe89363716de4a2088654fd9c1d30c5e712413
vladut96 <paygrishenko96@gmail.com>
2025-09-09 18:43:25 +0200

    Added and  fixed versel.json

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
+import { RequestListener } from 'http';
+
+let cachedApp: INestApplication | null = null;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.init(); // ⚡️ без listen
+    cachedApp = app;
+  }
+  return cachedApp.getHttpAdapter().getInstance() as RequestListener;
 }
-bootstrap();
+
+// Если запускаем локально → стандартный bootstrap с app.listen()
+if (!process.env.VERCEL) {
+  (async () => {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(process.env.PORT ?? 3000);
+    console.log(
+      `🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`,
+    );
+  })();
+}
+
+// Экспортируем handler для Vercel
+export default async function handler(req, res) {
+  const app = await bootstrap();
+  return app(req, res);
+}
